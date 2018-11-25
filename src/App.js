c5eb82dcf1c823899c17baa9817ad01c7e0b1738
Stephanie Coates <stcoates8@gmail.com>
2018-11-25 00:22:14 -0700

    popup hamburger menu complete

--- src/App.js
+++ src/App.js
@@ -11,30 +11,29 @@ import Popup from "reactjs-popup";
 import BurgerIcon from "./components/Menu/BurgerIcon";
 
 class App extends Component {
-  
+
   render() {
-      const contentStyle = {
-        background: "rgba(255,255,255,0)",
-        width: "80%",
-        border: "none"
-      };
+    const contentStyle = {
+      background: "rgba(255,255,255,0)",
+      width: "275px",
+      border: "none"
+    };
     return (
-      <div className="App"> 
-      {this.props.location.pathname === '/login' ? <Auth /> : 
-      <div>
-    <Popup
-      modal
-      overlayStyle={{ background: "rgba(255,255,255,0.7" }}
-      contentStyle={contentStyle}
-      closeOnDocumentClick={true}
-      trigger={open => <BurgerIcon open={open} />}
-    >
-      {close => <Menu close={close} />}
-    </Popup>
-      {/* <Menu /> */}
-      <Header/>
-       {routes}
-       <ActiveAlert/></div>}
+      <div className="App">
+        {this.props.location.pathname === '/login' ? <Auth /> :
+          <div>
+            <Popup
+              modal
+              overlayStyle={{ background: "rgba(255,255,255,0.9" }}
+              contentStyle={contentStyle}
+              closeOnDocumentClick={true}
+              trigger={open => <BurgerIcon open={open} />}
+            >
+              {close => <Menu close={close} />}
+            </Popup>
+            <Header />
+            {routes}
+            <ActiveAlert /></div>}
       </div>
     );
   }
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,27 +3,11 @@ import { Link } from 'react-router-dom';
 
 export default ({ close }) => (
   <div className="menu">
-    <ul>
-      <Link to='' onClick={close}>Home</Link>
-      <Link to='' onClick={close}>Getting Started</Link>
-      <Link to='' onClick={close}>Getting Started</Link>
-      <Link to='' onClick={close}>Getting Started</Link>
-      <Link to='' onClick={close}>Getting Started</Link>
-      <Link to='' onClick={close}>Getting Started</Link>
-     
-    </ul>
+      <Link className='menulink' to='/' onClick={close}>home</Link>
+      <Link className='menulink' to='/hikes' onClick={close}>find a hike</Link>
+      <Link className='menulink' to='/gear' onClick={close}>pack your pack</Link>
+      <Link className='menulink' to='/setalert' onClick={close}>set an alert</Link>
+      <Link className='menulink' to='/finalchecklist' onClick={close}>final checklist</Link>
+      <Link className='menulink' to='resources' onClick={close}>resources</Link>
   </div>
 );
-
-// export default props => {
-//   return (
-//     <Menu>
-//       <Link className='menu-item' to='/'>Home</Link>
-//       <Link to='/hikes'>Find A Hike</Link>
-//       <Link to='/gear'>Pack Your Pack</Link>
-//       <Link to='/setalert'>Set An Alert</Link>
-//       <Link to='/finalchecklist'>Final Checklist</Link>
-//       <Link to='/resources'>Resources</Link>
-//     </Menu>
-//   );
-// };
